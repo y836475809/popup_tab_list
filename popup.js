a9843261e95fe2f93ada5b0a05843b47b5b5664c
y836475809 <tttdaytttday@gmail.com>
2020-11-18 20:52:56 +0900

    リンクでなくdivのmouseupで動くように変更

--- popup.js
+++ popup.js
@@ -1,49 +1,47 @@
-function firstUnpinnedTab(tabs) {
-    for (var tab of tabs) {
-        if (!tab.pinned) {
-            return tab.index;
-        }
-    }
-}
+/* globals browser */
 
-/**
-    * listTabs to switch to
-    */
 function listTabs() {
     getCurrentWindowTabs().then((tabs) => {
-        let tabsList = document.getElementById('tabs-list');
-        let currentTabs = document.createDocumentFragment();
-        let limit = 50;
+        const tabs_list = document.getElementById("tabs-list");
+        const current_tabs = document.createDocumentFragment();
+        const limit = 500;
         let counter = 0;
 
-        tabsList.textContent = '';
-
         for (let tab of tabs) {
-            if (!tab.active && counter <= limit) {
-                let parent = document.createElement("div");
-                parent.classList.add('switch-tabs-div');
-                parent.title = tab.title;
-
-                let tabLink = document.createElement('a');
-
-                let beastImage = document.createElement("img");
-                beastImage.setAttribute("src", tab.favIconUrl);
-                // beastImage.style.height = "100vh";
-                beastImage.className = "beastify-image";
-                parent.appendChild(beastImage);
-
-                tabLink.textContent = tab.title || tab.id;
-                tabLink.setAttribute('href', tab.id);
-                tabLink.classList.add('switch-tabs');
-                parent.appendChild(tabLink);
-
-                currentTabs.appendChild(parent);
+            if(counter > limit){
+                break;
             }
+           
+            const parent = document.createElement("div");
+            parent.classList.add("switch-tabs-div");
+            parent.title = tab.title;
+
+            const tab_title = document.createElement("div");
+            tab_title.classList.add("switch-tabs");
+            tab_title.dataset.tabid = tab.id;
+            tab_title.textContent = tab.title || tab.id;
+            if (tab.active) {
+                tab_title.classList.add("switch-tabs-active");
+            }
+
+            let favicon = null;
+            if(tab.favIconUrl){
+                favicon = document.createElement("img");
+                favicon.setAttribute("src", tab.favIconUrl);
+            }else{
+                favicon = document.createElement("div");
+            }
+            favicon.dataset.tabid = tab.id;
+            favicon.className = "tab-favicon";
+
+            parent.appendChild(favicon); 
+            parent.appendChild(tab_title);
+            current_tabs.appendChild(parent);
 
             counter += 1;
         }
 
-        tabsList.appendChild(currentTabs);
+        tabs_list.appendChild(current_tabs);
     });
 }
 
@@ -53,14 +51,21 @@ function getCurrentWindowTabs() {
     return browser.tabs.query({ currentWindow: true });
 }
 
-document.addEventListener("click", (e) => {
-    if (e.target.classList.contains('switch-tabs')) {
-        var tabId = +e.target.getAttribute('href');
+document.addEventListener("mouseup", e => {
+    if(e.button == 1){
+        window.close();
+    }
+    if (e.target.classList.contains("switch-tabs")) {
+        const ac_elm = document.querySelector(".switch-tabs-active");
+        ac_elm.classList.remove("switch-tabs-active");
+
+        e.target.classList.add("switch-tabs-active");
+        const tabId = parseInt(e.target.dataset.tabid);
 
         browser.tabs.query({
             currentWindow: true
         }).then((tabs) => {
-            for (var tab of tabs) {
+            for (let tab of tabs) {
                 if (tab.id === tabId) {
                     browser.tabs.update(tabId, {
                         active: true
@@ -71,22 +76,4 @@ document.addEventListener("click", (e) => {
     }
 
     e.preventDefault();
-});
-
-//onRemoved listener. fired when tab is removed
-browser.tabs.onRemoved.addListener((tabId, removeInfo) => {
-    console.log(`The tab with id: ${tabId}, is closing`);
-
-    if (removeInfo.isWindowClosing) {
-        console.log(`Its window is also closing.`);
-    } else {
-        console.log(`Its window is not closing`);
-    }
-});
-
-//onMoved listener. fired when tab is moved into the same window
-browser.tabs.onMoved.addListener((tabId, moveInfo) => {
-    var startIndex = moveInfo.fromIndex;
-    var endIndex = moveInfo.toIndex;
-    console.log(`Tab with id: ${tabId} moved from index: ${startIndex} to index: ${endIndex}`);
 });
