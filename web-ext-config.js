54e212a39eb3609ad3d5886ec13a83b82165d542
y836475809 <tttdaytttday@gmail.com>
2020-11-21 21:36:53 +0900

    web-extを使用する

---
+++ web-ext-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    build: {
+        overwriteDest: true
+    },
+    ignoreFiles: [
+        "image",
+        "tmp",
+        "package.json",
+        "package-lock.json",
+        "README.md",
+        "web-ext-config.js"
+    ]
+};
